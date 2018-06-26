74ff0937d60f289af7835fa3745983f5f2e2cabb
shu251 <sarah.hu@usc.edu>
2018-06-25 17:13:51 -0700

    updated with additional analyses

--- OTUtable_to_PrelimFigs_R.r
+++ OTUtable_to_PrelimFigs_R.r
@@ -18,7 +18,7 @@ length(count$OTU.ID) #Total number of OTUs generated
 colsum<-apply(count[2:6],2,sum) #Colums 2:5 are my sample colums
 colsum #number of sequences per sample
 
-##Filter out OTUs with only 1 sequence, singletons
+##Filter out OTUs with only 1 sequence in the whole dataset (global singletons)
 rowsum<-apply(count[2:6],1,sum) #remove global singletons.
 count.no1 = count[ rowsum>1, ]  #count.no1 = OTU table without global singletons
 dim(count)[1] - dim(count.no1)[1] #Outputs the number of OTUs (total) lost in this step
@@ -57,9 +57,9 @@ bar_stats %+% subset(allM, Figure %in% "Sequences")+labs(title="Total number of 
 
 #Based on the total number of sequences in each sample.
 count.no1$Sample_2<-NULL
-head(count.no1)
+head(count.no1[1:3,])
 
-#Optional code to subsample
+# Randomly subsample data
 #Assign row names as OTU IDs
 row.names(count.no1)<-count.no1$OTU.ID
 #Isolate only columns with data
@@ -70,6 +70,11 @@ sub<-min(colSums(keep)); sub #sub =total number of sequences that is fewest amon
 rare <- rrarefy(t(keep), sub) #Randomly subsamples data so that all sample have the same number of sequences
 subsampled<-as.data.frame(t(rare))
 colSums(subsampled) #all should be equal to sub (in test data, n=99,530)
+
+# Perform centered log-ratio normalization:
+# library(compositions)
+# df_clr<-as.data.frame(clr(keep))
+## Also look into DESeq for additional options for normalization
 
 #Calculate alpha diversity
 
@@ -88,14 +93,27 @@ head(alpha.m)
 
 ggplot(alpha.m, aes(x=samples, y=value, fill=variable, shape=variable))+geom_point(size=4, aes(color=variable))+facet_grid(variable~.,scales="free")+theme_bw()+theme(axis.text.x = element_text(angle = 90,hjust=1,vjust=0.5,size=8),axis.text.y=element_text(size=12),legend.position = "top")
 
+# Use the subsampled data
+head(subsampled[1:3,])
+
+tmp<-subsampled[1:4] #select only numerics
+curve_in<-as.data.frame(t(tmp)) # transpose
+
+# graph parameters:
+colors<-c("red", "orange", "lightblue", "darkgreen")
+linetype<-c("dotted", "solid", "solid", "dotted")
+
+rarefac<-rarecurve(curve_in, step=20, lwd=3, col=colors, lty=linetype, label=F, xlab="Number of reads sampled", ylab="Number of OTUs")
+
 #head(count.no1) #original data, has OTU and taxonomy information
 #head(subsampled) #subsampled data for analysis
 key<-count.no1[c(1,6)] #OTU ID to taxonomy key
 subsampled$OTU.ID<-row.names(subsampled)
+head(key)
 
 #Get taxonomic information (by OTU.ID) back on subsampled data
 count.subsampled<-join(subsampled, count.no1[c(1,6)], by="OTU.ID", type="left", match="first")
-head(count.subsampled); dim(count.subsampled)
+head(count.subsampled[1:3,]); dim(count.subsampled)
 
 #This is specific to the PR2 database. 
 #Depending on your scientific question, you will need to review the taxonomic grouping
@@ -149,7 +167,7 @@ data_binned<-data.frame(count.subsampled, newtax)
 data.m<-melt(data_binned) #melt
 #head(data.m)
 data.agg<-aggregate(data.m$value, by=list(Taxa=data.m$Taxa,Samples=data.m$variable),sum) #sum sequences by taxonomic group
-save(data.agg, data.m, data_binned, file="Checkpoint2_PrelimFigs.Rdata")
+# save(data.agg, data.m, data_binned, file="Checkpoint2_PrelimFigs.Rdata")
 
 tax_order=c("Alveolates-Ciliates","Alveolates-Dinoflagellates","Alveolates-Syndiniales","Alveolates-Other","Archaeplastids-Chlorophytes","Archaeplastids-Other","Cryptophytes","Excavates","Haptophytes","Rhizaria-Acantharia","Rhizaria-Cercozoa","Rhizaria-Polycystines","Rhizaria-RAD (A,B,C)","Stramenopiles-Diatoms","Stramenopiles-Pelagophytes","Stramenopiles-MAST","Stramenopiles-Chrysophytes","Stramenopiles-Other","Opisthokont-Fungi","Opisthokont-Metazoa","Opisthokonts-Other","Other/unknown","Unassigned")
 tax_color=c("#800026","#cb181d","#e7298a","#df65b0","#fc4e2a","#fd8d3c","#fed976","#c7e9b4","#7fcdbb","#41ae76","#238b45","#006d2c","#00441b","#c6dbef","#6baed6","#1d91c0","#225ea8","#253494","#081d58","#54278f","#8c510a","#bf812d","#dfc27d")
@@ -158,5 +176,83 @@ data.agg$tax<-factor(data.agg$Taxa, levels=rev(tax_order)) #factoring
 head(data.agg)
 
 #Bar plot of community composition
-ggplot(data.agg[order(data.agg$tax),], aes(y=x,fill=tax,x=Samples))+geom_bar(position = "fill", stat = "identity", color="black",aes(fill=tax))+scale_fill_manual(values=tax_color)+labs(title="", x="",y="Relative abundance of reads")+theme_bw()+theme(legend.position="right",axis.text.x = element_text(angle=45, hjust=1,vjust=1,color="black"))
-#Change position = "fill" to position="stack" to look at total abundance
+ggplot(data.agg[order(data.agg$tax),], aes(y=x,fill=tax,x=Samples))+
+    geom_bar(position = "fill", stat = "identity", color="black",aes(fill=tax))+
+    scale_fill_manual(values=tax_color)+
+    labs(title="", x="",y="Relative abundance of reads")+
+    theme_bw()+
+    theme(legend.position="right",axis.text.x = element_text(angle=45, hjust=1,vjust=1,color="black"))
+
+# We can plot this again, but look at total abundance of only diatoms
+# Changed position="stack" and set plot equal to "barplot"
+barplot<-ggplot(data.agg[order(data.agg$tax),], aes(y=x,fill=tax,x=Samples))+
+    geom_bar(position = "stack", stat = "identity", color="black",aes(fill=tax))+
+    scale_fill_manual(values=tax_color)+
+    labs(title="", x="",y="Total reads")+
+    theme_bw()+
+    theme(legend.position="right",axis.text.x = element_text(angle=45, hjust=1,vjust=1,color="black"))
+# select only diatoms to plot:
+barplot %+% subset(data.agg, tax %in% "Stramenopiles-Diatoms")
+# Repeate but with all stramenopiles:
+stram<-c("Stramenopiles-Diatoms", "Stramenopiles-MAST","Stramenopiles-Pelagophytes", "Stramenopiles-Chrysophytes")
+barplot %+% subset(data.agg, tax %in% stram)
+
+# Area plot
+# make sure to use "as.numeric" for the x-axis
+# note that the x-axis automatically changes to 1-4. 
+# Ideally, re-label ahead of ploting
+plot_area_sub<-ggplot(data.agg[order(data.agg$tax),], aes(y=x,fill=tax,order=tax))+ 
+  geom_area(aes(fill= tax,x=as.numeric(Samples), color=tax), position = "fill", stat = "identity")+
+  scale_color_manual(values=tax_color)+
+  scale_fill_manual(values=tax_color)+
+  labs(title="", x="",y="Relative abundance of reads")+
+  theme(legend.position="right",legend.title = element_blank(),plot.title=element_text(hjust = 0,face='bold',size=20))+
+  theme(panel.grid.minor = element_blank(),panel.background = element_blank(),strip.background = element_blank(), axis.text.x = element_text(angle=45,hjust = 1,vjust = 1, color = "black"), axis.text.y=element_text(color="black"))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank())+
+  theme(panel.border = element_blank(), axis.line = element_line())+
+  scale_y_continuous(expand = c(0, 0)) 
+plot_area_sub
+
+# use subsampled data
+head(subsampled[1:2,])
+num_only<-subsampled[1:4] # select numerics only
+
+# Again (since re removed Sample 2) remove whole rows that equal zero
+rowsum<-apply(num_only,1,sum)
+num_only_no0 = num_only[ rowsum>0, ]
+dim(num_only_no0)
+
+# Normalize by calculating relative abundance
+#tmp<-num_only_no0[1:200,] # take the top 200 OTUs (to reduce run time of NMDS for this tutorial)
+relabun <- decostand(num_only_no0, MARGIN=2, method = "total")
+colSums(relabun) # check! should all equal 1.
+t.relabun<-t(relabun) # transform
+
+# Cluster dendrogram:
+cluster<-hclust(dist(t.relabun), method="average")
+plot(cluster)
+
+# Calculate MDS with Bray-Curtis dissimilarity matrix
+NMDS=metaMDS(t.relabun,distance="bray",k=2)
+# Because this is trial data, an error will be reported. This should not happen with larger datasets
+#head(NMDS$points)
+#head(NMDS$stress)
+#stressplot(NMDS)
+# Note that this approach will generate positions of samples (communities) in a multidimensional space using a reduced number of dimensions (2) for visualization purposes.
+
+# Basic option to plot:
+plot(NMDS)
+orditorp(NMDS,display="sites",col="blue") #label samples
+#black circles are the OTUs
+
+# Alternatively, you can extract the points for each sample and plot using ggplot2
+NMDS_pts <- NMDS$points[1:nrow(NMDS$points),]
+NMDS_pts<-as.data.frame(NMDS_pts)
+NMDS_pts$Sample<-row.names(NMDS_pts)
+# head(NMDS_pts)
+
+NMDS_plot <- ggplot(NMDS_pts, aes(x = MDS1, y = MDS2, shape=Sample, fill=Sample), color="black") + 
+    geom_point(size=6,aes(fill=Sample)) + 
+    theme_bw() +
+    scale_shape_manual(values = c(21,22,21,22))+scale_fill_manual(values=c("red","red", "black", "black"))
+NMDS_plot
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Creating a preliminary set of figures for 18S tag sequencing
-Purpose of this is to generate a preliminary set of figures (and a table!) that give you a first look of your new tag sequence data. As written it is specific to 18S tag-sequencing projects using the PR2 database. But, of course can be modified for other tag sequencing results.
-Originally created for an in-lab tutorial for evaluating tag sequencing data.
+# Generate a preliminary set of figures from tag sequencing data
+The purpose of this is to create a preliminary set of figures (and a table!) in R. This will give you a first look of your new tag sequence data. As written it is specific to 18S tag-sequencing projects using the PR2 database. But, of course can be modified for other tag sequencing results.
+Originally created for an in-lab tutorial to demonstrate the use of R, provie an example of initial R-based analyses to apply to tag sequence results, and generate figures to ward off an anxious PI.
 
 ## Requirements
 * R v3.3.2 or higher
 * (optional) Jupyter notebook (R)
-* To follow directly, use product from [OTU clustering in qiime1 tutorial](https://github.com/shu251/V4_tagsequencing_18Sdiversity_q1). Otherwise, ensure you have an OTU table, where each row is an OTU (with a unqiue ID) and each column is a sample (first row is a header/sample name) or follow along with provided text file: 'V4_OTUtable_test.txt'
+* To follow directly, use products from [OTU clustering in qiime1 tutorial](https://github.com/shu251/V4_tagsequencing_18Sdiversity_q1) or [OTU clusering in qiime2](https://github.com/shu251/V4_tagsequencing_18Sdiversity_qiime2). Otherwise, ensure you have an OTU table, where each row is an OTU (with a unqiue ID) and each column is a sample (first row is a header/sample name) or follow along with provided text file: 'V4_OTUtable_test.txt'
 
 ## Jupyter R notebook OTUtable_to_PrelimFigs_R
 
@@ -27,10 +27,20 @@ Note that the script removes global singletons (OTUs with only one sequence in t
 
 Next the R script walks the user through removing unwanted samples (based on your preliminary look at the data). For instance, if one of your samples has too few sequences or too few OTUs, this is where you can remove that sample.
 
+### Notes on normalization
+The product of OTU or ASV clustering will be compositional data. There are better and better methods for dealing with this type of data, but at the end of the day, always keep this in mind with your interpretations.
+
+Recommended references:
+* Gloor, G. B., Macklaim, J. M., Pawlowsky-Glahn, V. & Egozcue, J. J. Microbiome Datasets Are Compositional: And This Is Not Optional. Front. Microbiol. 8, 57–6 (2017).
+* Weiss, S. et al. Normalization and microbial differential abundance strategies depend upon data characteristics. Microbiome 5, 1–18 (2017).
+* McMurdie, P. J. & Holmes, S. Waste Not, Want Not: Why Rarefying Microbiome Data Is Inadmissible. PLoS Comput Biol 10, e1003531 (2014).
+
+For this example in R, we will stick with randomly subsampling our data. This normalization step serves to account for sample-to-sample differences in the raw sequence results. I'll continue updating the R script here as much as I can. 
+
 ## Randomly subsample data
 It is required that you randomly subsample data so that all samples have the same number of sequences for any calculation of diversity estimates. Again, depending on your question, it may also be advisable to use subsampled data for all downstream analyses. Here, I'm using the vegan package to do this. 
 
-## Calculate alpha diversity
+## Calculate basic alpha diversity indices
 Using the vegan package, I calculated Shannon and inverse Simpsons diversity metrics, and the total number of OTUs per sample (as a proxy for species richness).
 https://cran.r-project.org/web/packages/vegan/vegan.pdf
 
@@ -38,13 +48,25 @@ Comparing diversity metrics between samples
 ![alpha](https://github.com/shu251/figs/blob/master/alpha_div.png)
 In this test example, Sample 1 appears to be more diverse than the other samples.
 
+## Generate rarefaction curve
+This type of figure gives you an *idea* of how many OTUs you should *expect*. By continually subsampling your data (x-axis), how many OTUs do you get as a result (y-axis). *Ideally*, sampling/sequencing effort can be called good enough if your rarefaction curve reaches an asymptote. 
+
+The example provided here does not even out because I've subsampled the data for run time.
+
+![rare](https://github.com/shu251/figs/blob/master/rare.png)
+
 ## Simplifying PR2 taxonomy output and making a community composition plot 
 The output from the PR2 database has a long list of taxa to the approximately species name. These next steps demonstrate how you can get a quick view of the whole community at the approximately phylum or class level. However, depending on your project/biome/advisor/personal preference, you are likely going to want to highlight specific groups or NOT highlight certain taxonomic groups. Since my lab focused on several of the major taxonomic groups found in marine ecosystems (like off the coast of SoCal!), I created a function to simplify the taxonomic names. This is a great way to get a 'birds eye' view of the data and give your advisor a general idea of what the whole community looks like.
-Function specifically separates out the PR2 output into Levels and then takes the information from the various levels to fill in a newly created "Taxa" column. Then I aggregate the data, or sum the data, by the taxonomic group designated by "Taxa". 
+This R function specifically separates out the PR2 output into Levels and then takes the information from the various levels to fill in a newly created "Taxa" column. Then I aggregate the data, or sum the data, by the taxonomic group designated by "Taxa". 
 
-## Community composition - relative abundance of sequences
+## Community composition - plotting
+
+Several options for plotting community composition using ggplot. I've included barplots representing the relative abundance of reads (pictured below), barplots that demonstrate total reads for a given taxonomic group, and and area plot.  
 
 ![Community composition](https://github.com/shu251/figs/blob/master/CommunityComposition.png)
 
+## Application of Bray-Curtis dissimilarity
+This is typically used to evaluate the beta diversity of a data set. In this short example, the relative abundance of OTUs in each sample was computed. Then I demonstrate two ways to visualize the data, first to generate an average hierarchical cluster dendrogram. Then use the vegan package command "metaMDS()" to calculate a Bray-Curtis dissimilarity matrix and plot in multidimensional space (using either base R plot or extracting the points from the NMDS and plotting in ggplot). 
 
+# Last updated June 25th, 2018 - S. Hu
 
